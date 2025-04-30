c5eec92e0442d071d01699135b2055905cb9de97
Bobby Yun <cyun2@charlotte.edu>
2025-04-30 04:04:42 -0400

    s

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 400);
+    background(220);
+  }
+  
+  function draw() {
+    fill(0, 150, 255);
+    stroke(0);
+    strokeWeight(4);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+  
